6106797b6cd8bec138a5be5c28c89da45d4fe887
macmann <davidkaung@gmail.com>
2025-12-28 21:00:32 +0630

    Add pages document and minimal error pages

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>404</h1>
+    </main>
+  );
+}
---
+++ pages/500.tsx
@@ -0,0 +1,7 @@
+export default function ServerErrorPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>500</h1>
+    </main>
+  );
+}
