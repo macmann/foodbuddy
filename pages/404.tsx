703ca5fad51567017b0c6add3a499d0c356ecd78
macmann <davidkaung@gmail.com>
2025-12-28 22:17:20 +0630

    Remove custom 404/500 pages

--- pages/404.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFoundPage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>404</h1>
-    </main>
-  );
-}
--- pages/500.tsx
+++
@@ -1,7 +0,0 @@
-export default function ServerErrorPage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>500</h1>
-    </main>
-  );
-}
